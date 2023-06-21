7fdbfd035814265d62a95d0fcef6ad93ab80466e
Norbert Wowy <norbert.wowy@gmail.com>
2023-06-21 17:26:35 +0200

    Adding a for loop
    Showing names of pokemon and thereafter it's height in round brackets

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
-let pokemonList=[
-    {name: "bulbasaur", height: "7", type: "grass"},
-    {name: "charizard", height: "6", type: "fire"},
-    {name: "squirtle", height: "5", type: "water"}
-];
+    
+    let pokemonList=[
+        {name: "bulbasaur", height: "7", type: "grass"},
+        {name: "charizard", height: "6", type: "fire"},
+        {name: "squirtle", height: "5", type: "water"}
+    ];
+    
+    for (let i=0; i < pokemonList.length; i++)
+    { console.log(pokemonList[i].name + " (height: " + pokemonList[i].height +")")
+    }  
